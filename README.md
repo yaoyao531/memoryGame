cd60ea398aa59800bed1a11f0ede6d6929b9b504
Chen Guo <guochen1223@gmail.com>
2018-12-22 01:52:32 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## Project title
+Memory Game
+
+## Motivation
+Combines HTML, CSS, and JavaScript to write a fully functional memory game for demo
+
+## Code style
+Standard
+
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
+
+## Tech/framework used
+CSS Bootstrap
+
+## Features
+1. A timer that indicates how much time left before you can finish the game
+2. A flip counter that indicates how many flips you have made
+3. If either timer runs out or flips reach max flips, you lose the game
+4. If you find all matches before timer runs out or flips reach max, you win the game
+5. Either you win or lose, the game is reset after that
+
+## How to use?
+Run index.html in modern browser and play the game
+
+## Contribute
+
+## Credits
+Special thanks to Chen Guo https://github.com/vesperiaLSU
---
+++ js/script.js
@@ -0,0 +1,166 @@
+// global variables
+const inputs = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'];
+let clickedObjects = [];
+let seconds = 90;
+let flips = 0;
+let toggleBtn = true;
+let timer;
+let counter = 0;
+const maxFlips = 60;
+
+
+// run after page is loaded
+window.onload = function () {
+    const startButton = document.getElementById("start-btn");
+    startButton.addEventListener('click', startGame);
+    const resetButton = document.getElementById("reset-btn");
+    resetButton.addEventListener('click', resetGame);
+}
+
+// Utility functions
+function startGame() {
+    shuffleInputs(inputs);
+    initGame();
+    initTimer();
+    toggleButton();
+}
+
+function resetGame() {
+    revertGame();
+    resetTimer();
+    resetFlips();
+    toggleButton();
+}
+
+function initTimer() {
+    timer = setInterval(function () {
+        document.getElementById("countdown").innerHTML = `${seconds}s`;
+        seconds--;
+        // If the count down is over, write some text 
+        if (seconds < 0) {
+            clearInterval(timer);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+            setTimeout(function () {
+                window.confirm('Time is out! Try again?');
+                resetGame();
+            }, 500);
+        }
+    }, 1000);
+}
+
+function initGame() {
+    const gameCards = document.getElementsByClassName("game_card");
+    for (let i in gameCards) {
+        if (typeof (gameCards[i]) === 'object') {
+            gameCards[i].addEventListener('click', clickGameCard);
+            gameCards[i].setAttribute("data-value", inputs[i]);
+            gameCards[i].setAttribute("id", `card-${i}`);
+        }
+    }
+}
+
+function toggleButton() {
+    const startButton = document.getElementById("start-btn");
+    const resetButton = document.getElementById("reset-btn");
+    if (toggleBtn) {
+        startButton.style.display = "none";
+        resetButton.style.display = "inline";
+    } else {
+        startButton.style.display = "inline";
+        resetButton.style.display = "none";
+    }
+    toggleBtn = !toggleBtn;
+}
+
+function clickGameCard(elem) {
+    let card = elem.target;
+    if (card.nodeName === 'DIV') {
+        increaseFlips();
+        if (flips === maxFlips) {
+            window.confirm('Reach max flips! Try again?');
+            resetGame();
+            return;
+        }
+        const cardValue = card.getAttribute('data-value');
+        const cardId = card.getAttribute('id');
+        if (clickedObjects.length === 1 && cardId === clickedObjects[0].id) {
+            return;
+        }
+        card.innerHTML = `<span class="letter">${cardValue}</span>`;
+        clickedObjects.push({ value: cardValue, id: cardId });
+        if (clickedObjects.length === 2) {
+            const obj1 = clickedObjects[0];
+            const obj2 = clickedObjects[1];
+            const div1 = document.getElementById(obj1.id);
+            const div2 = document.getElementById(obj2.id);
+            if (obj1.value === obj2.value) {
+                div1.style.backgroundColor = 'blue';
+                div2.style.backgroundColor = 'blue';
+                div1.removeEventListener('click', clickGameCard);
+                div2.removeEventListener('click', clickGameCard);
+                setTimeout(function () {
+                    counter++;
+                    if (counter === 8) {
+                        const winningMsg = `You Win!! You used ${flips} flips, and ${90 - seconds} seconds`;
+                        window.confirm(winningMsg);
+                        resetGame();
+                    }
+                }, 500);
+            } else {
+                div1.style.backgroundColor = 'red';
+                div2.style.backgroundColor = 'red';
+                setTimeout(() => {
+                    div1.innerHTML = null;
+                    div2.innerHTML = null;
+                    div1.style.backgroundColor = 'darkslategray';
+                    div2.style.backgroundColor = 'darkslategray';
+                }, 1000);
+            }
+            clickedObjects.splice(0, clickedObjects.length);
+        } else {
+            const obj1 = clickedObjects[0];
+            const div1 = document.getElementById(obj1.id);
+            div1.style.backgroundColor = 'green';
+        }
+    }
+}
+
+function increaseFlips() {
+    flips++;
+    document.getElementById("count").innerHTML = `${flips}`;
+}
+
+function revertGame() {
+    const gameCards = document.getElementsByClassName("game_card");
+    clickedObjects.splice(0, clickedObjects.length);
+    for (let i in gameCards) {
+        if (typeof (gameCards[i]) === 'object') {
+            gameCards[i].removeEventListener('click', clickGameCard);
+            gameCards[i].removeAttribute("data-value", inputs[i]);
+            gameCards[i].removeAttribute("id", `card-${i}`);
+            gameCards[i].innerHTML = '';
+            gameCards[i].style.backgroundColor = 'darkslategray';
+        }
+    }
+}
+
+function resetTimer() {
+    clearInterval(timer);
+    seconds = 90;
+    document.getElementById("countdown").innerHTML = `${seconds}s`;
+}
+
+function resetFlips() {
+    flips = 0;
+    document.getElementById("count").innerHTML = `${flips}`;
+}
+
+function shuffleInputs(inputs) {
+    for (let i in inputs) {
+        const swapIdx = Math.trunc(Math.random() * inputs.length);
+        const tmp = inputs[swapIdx];
+        inputs[swapIdx] = inputs[i];
+        inputs[i] = tmp;
+    }
+}
+
