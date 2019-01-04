ebc28def2e9c5bab9fe7d2f1e63261718bbe3687
Chen Guo <guochen1223@gmail.com>
2019-01-03 22:48:51 -0600

    resubmit

--- js/script.js
+++ js/script.js
@@ -17,26 +17,30 @@ window.onload = function () {
     resetButton.addEventListener('click', resetGame);
 }
 
-// Utility functions
+// Start the game by shuffling input, initing game, timer and toggling the start button
 function startGame() {
     shuffleInputs(inputs);
     initGame();
     initTimer();
     toggleButton();
-    hideStar();
 }
 
+// When the game restarts, reset game, timer, flips, and star rating. Toggle the start button.
 function resetGame() {
     revertGame();
     resetTimer();
     resetFlips();
+    showStar(3);
     toggleButton();
 }
 
+// Initiate the timer and update the star rating every second.
 function initTimer() {
     timer = setInterval(function () {
         document.getElementById("countdown").innerHTML = `${seconds}s`;
         seconds--;
+        const rating = calRatings();
+        showStar(rating);
         // If the count down is over, write some text 
         if (seconds < 0) {
             clearInterval(timer);
@@ -49,6 +53,7 @@ function initTimer() {
     }, 1000);
 }
 
+// Initiate the game board with values. 
 function initGame() {
     const gameCards = document.getElementsByClassName("game_card");
     for (let i in gameCards) {
@@ -60,6 +65,7 @@ function initGame() {
     }
 }
 
+// Show or hide the Start and Reset game button. 
 function toggleButton() {
     const startButton = document.getElementById("start-btn");
     const resetButton = document.getElementById("reset-btn");
@@ -73,6 +79,7 @@ function toggleButton() {
     toggleBtn = !toggleBtn;
 }
 
+// When game card is clicked, the flips are increased. The cards clicked are compared and evaluated. 
 function clickGameCard(elem) {
     let card = elem.target;
     if (card.nodeName === 'DIV') {
@@ -90,37 +97,7 @@ function clickGameCard(elem) {
         card.innerHTML = `<span class="letter">${cardValue}</span>`;
         clickedObjects.push({ value: cardValue, id: cardId });
         if (clickedObjects.length === 2) {
-            const obj1 = clickedObjects[0];
-            const obj2 = clickedObjects[1];
-            const div1 = document.getElementById(obj1.id);
-            const div2 = document.getElementById(obj2.id);
-            if (obj1.value === obj2.value) {
-                div1.style.backgroundColor = 'blue';
-                div2.style.backgroundColor = 'blue';
-                div1.removeEventListener('click', clickGameCard);
-                div2.removeEventListener('click', clickGameCard);
-                
-                setTimeout(function () {
-                    counter++;
-                    if (counter === 8) {
-                        const rating = calRatings();
-                        showStar(rating);
-                        const winningMsg = `You Win!! You used ${flips} flips, and ${90 - seconds} seconds`;
-                        window.confirm(winningMsg);
-                        resetGame();
-                    }
-                }, 1000);
-            } else {
-                div1.style.backgroundColor = 'red';
-                div2.style.backgroundColor = 'red';
-                setTimeout(() => {
-                    div1.innerHTML = null;
-                    div2.innerHTML = null;
-                    div1.style.backgroundColor = 'darkslategray';
-                    div2.style.backgroundColor = 'darkslategray';
-                }, 1000);
-            }
-            clickedObjects.splice(0, clickedObjects.length);
+            handleEqualValue();
         } else {
             const obj1 = clickedObjects[0];
             const div1 = document.getElementById(obj1.id);
@@ -129,11 +106,51 @@ function clickGameCard(elem) {
     }
 }
 
+// Handles when the two cards have the same value. 
+function handleEqualValue() {
+    const obj1 = clickedObjects[0];
+    const obj2 = clickedObjects[1];
+    const div1 = document.getElementById(obj1.id);
+    const div2 = document.getElementById(obj2.id);
+    if (obj1.value === obj2.value) {
+        div1.style.backgroundColor = 'blue';
+        div2.style.backgroundColor = 'blue';
+        div1.removeEventListener('click', clickGameCard);
+        div2.removeEventListener('click', clickGameCard);
+
+        setTimeout(function () {
+            counter++;
+            if (counter === 8) {
+                const rating = calRatings();
+                const stars = rating > 1 ? 'stars' : 'star';
+                showStar(rating);
+                const winningMsg = `You Win!! You used ${flips} flips, and ${90 - seconds} seconds. Your overall rating is ${rating} ${stars}`;
+                window.confirm(winningMsg);
+                resetGame();
+            }
+        }, 1000);
+    } else {
+        div1.style.backgroundColor = 'red';
+        div2.style.backgroundColor = 'red';
+        setTimeout(() => {
+            div1.innerHTML = null;
+            div2.innerHTML = null;
+            div1.style.backgroundColor = 'darkslategray';
+            div2.style.backgroundColor = 'darkslategray';
+        }, 1000);
+    }
+    clickedObjects.splice(0, clickedObjects.length);
+}
+
+// Increase the total counts of flips.
 function increaseFlips() {
     flips++;
     document.getElementById("count").innerHTML = `${flips}`;
+    const rating = calRatings();
+    showStar(rating);
 }
 
+// Revert game to the original state. 
 function revertGame() {
     const gameCards = document.getElementsByClassName("game_card");
     clickedObjects.splice(0, clickedObjects.length);
@@ -149,17 +166,20 @@ function revertGame() {
     }
 }
 
+// Reset the timer to 90 seconds. 
 function resetTimer() {
     clearInterval(timer);
     seconds = 90;
     document.getElementById("countdown").innerHTML = `${seconds}s`;
 }
 
+// Reset the flips. 
 function resetFlips() {
     flips = 0;
     document.getElementById("count").innerHTML = `${flips}`;
 }
 
+// Shuffle cards. 
 function shuffleInputs(inputs) {
     for (let i in inputs) {
         const swapIdx = Math.trunc(Math.random() * inputs.length);
@@ -169,6 +189,7 @@ function shuffleInputs(inputs) {
     }
 }
 
+// Calculate the rating based on time used and flips. 
 function calRatings() {
     if (flips <= 20 && seconds >= 60) {
         return 3;
@@ -178,10 +199,12 @@ function calRatings() {
     return 1;
 }
 
+// Update star rating. 
 function showStar(rating) {
     const gameRating = document.getElementById("ratings");
+    gameRating.innerHTML = '';
     const title = document.createElement('h2');
-    title.innerHTML = 'Previous Game Rating';
+    title.innerHTML = 'Rating';
     gameRating.appendChild(title);
     if (gameRating == null) return;
     for (let i = 0; i < rating; i++) {
@@ -190,10 +213,4 @@ function showStar(rating) {
         gameRating.appendChild(star);
     }
     gameRating.style.display = 'block';
-}
-
-function hideStar() {
-    const gameRating = document.getElementById("ratings");
-    gameRating.innerHTML = "";
-    gameRating.style.display = 'none';
 }
